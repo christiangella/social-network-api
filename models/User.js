459239e5e5aaad2a5ffde91b4f574a6a5f0c6c80
christiangella <112913805+christiangella@users.noreply.github.com>
2023-02-11 16:03:29 -0800

    defining models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+// implementing unique validator (later)
+var uniqueValidator = require('mongoose-unique-validator')
+// importing mongoose for instance of schema, model
+const { Schema, model } = require('mongoose')
+
+// creating User schema & defining properties
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please input a valid email address.']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('user', userSchema)
+module.exports = User
