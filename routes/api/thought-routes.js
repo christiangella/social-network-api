8b0ae912696d103944605c73fbc5b5a8137d55be
christiangella <112913805+christiangella@users.noreply.github.com>
2023-02-13 17:55:35 -0800

    connecting backend

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,3 +3,21 @@ const router = require('express').Router()
 const {
     getAllThoughts, getOneThought, createThought, updateThought, deleteThought, addReaction, removeReaction
 } = require('../../controllers/thoughtControl')
+
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought)
+
+router.route('/:thoughtId')
+    .get(getOneThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router.route('/:thoughtId/reactions')
+    .post(addReaction)
+
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction)
+
+
+module.exports = router
